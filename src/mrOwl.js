fbf7b1298bbf57fc3416062b02f79be121260269
mostafa rastegar <m.rastegar991@gmail.com>
2020-01-03 13:29:50 +0330

    Fix css not found bug

--- src/mrOwl.js
+++ src/mrOwl.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import $ from "jquery";
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
