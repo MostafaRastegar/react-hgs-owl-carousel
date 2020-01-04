28bece64e60ea208ea106ff40a4b0517d2b1af1e
mostafa rastegar <mostafa.rastegar@snapp.market>
2020-01-04 17:05:32 +0330

    Add build folder on git

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './mrOwl.css';
-import mrOwl from './mrOwl';
+import mrOwl from './components';
 
 
 const MainSlider = () => {
